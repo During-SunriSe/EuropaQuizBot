fbd9069b71350d06610843ff8aa2f230ac7fe8f7
Bohdan Popov <bodyaluck2005@gmail.com>
2023-06-02 07:53:13 +0200

    text updated, added date from Users res

--- users/sheetsInfo.js
+++ users/sheetsInfo.js
@@ -58,6 +58,7 @@ export async function setInfo() {
               ...curUser.curPoints,
               curUser.points,
               curUser.note,
+              curUser.date,
             ],
           ],
         },
